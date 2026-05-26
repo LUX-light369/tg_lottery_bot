786c6e2aefe8794b6fec6e6f29706f73485f9dad
LUX-light369 <abramivan131@gmail.com>
2026-05-27 05:58:57 +0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,14 @@
+import zoneinfo
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from config import DATABASE_URL
+from config import DATABASE_URL, DEFAULT_TZ
 
-# Храним задачи в SQL, чтобы они не стирались при перезапуске бота
 job_stores = {
     'default': SQLAlchemyJobStore(url=DATABASE_URL.replace("sqlite+aiosqlite", "sqlite"))
 }
 
-scheduler = AsyncIOScheduler(jobstores=job_stores)
+# Передаем таймзону Новосибирска в планировщик задач
+scheduler = AsyncIOScheduler(
+    jobstores=job_stores, 
+    timezone=zoneinfo.ZoneInfo(DEFAULT_TZ)
+)
