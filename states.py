2bff3f347375050211310c7f9e20c5787f786be7
LUX-light369 <abramivan131@gmail.com>
2026-05-28 22:30:05 +0700

    Delete states.py

--- states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class LotterySetup(StatesGroup):
-    choosing_mode = State()       # Выбор: Время или Участники
-    entering_limit = State()      # Ввод минут или количества людей
-    entering_winners = State()    # Ввод количества победителей
-    entering_link = State()       # Ввод ссылки на канал/задание (обязательная подписка)
-    entering_chat_id = State()    # Ввод ID группы, куда отправить розыгрыш
