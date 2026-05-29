a649f2a964937db5e32b03ab4b40d51a22035f7b
LUX-light369 <abramivan131@gmail.com>
2026-05-30 03:53:25 +0700

    Delete middleware directory

--- middleware/session.py
+++
@@ -1,8 +0,0 @@
-from aiogram import BaseMiddleware
-from database.engine import async_session
-
-class DatabaseSessionMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event, data):
-        async with async_session() as session:
-            data["session"] = session
-            return await handler(event, data)
